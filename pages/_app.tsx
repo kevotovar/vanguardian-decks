f2c0feee1ffae147aee622d4437423deefec61ad
Kevin Tovar <kevintovar@esimez.mx>
2020-12-08 21:26:01 -0600

    Add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../lib/apolloClient'
 
+import '../styles/global.css'
+
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
